5bf177e5265cce695753718d18350d356e63cf52
Sadiq Jaffer <sadiq@toao.com>
2021-10-23 23:17:23 +0100

    wip

--- src/test_atomics.ml
+++ src/test_atomics.ml
@@ -1,10 +1,44 @@
 module Atomic = TracedAtomic
 
-let do_test () =
-  let a = Atomic.make 0 in
-  let b = Atomic.get a in
-  let c = Atomic.set a b in
-  ignore(c)
+(* a simple concurrent list *)
+
+type conc_list = { value: int; next: conc_list option }
+
+let rec add_node list_head n =
+  (* try to add a new node to head *)
+  let old_head = Atomic.get list_head in
+  let new_node = { value = n ; next = (Some old_head) } in
+      if not(Atomic.compare_and_set list_head old_head new_node) then
+        begin
+          add_node list_head n
+        end
+      else
+        begin
+          true
+        end
+
+let check_node list_head n =
+  let rec check_from_node node =
+    match (node.value, node.next) with
+    | (v, _) when v = n -> true
+    | (_, None) -> false
+    | (_ , Some(next_node)) -> begin
+      check_from_node next_node
+    end
+  in
+  (* try to find the node *)
+    check_from_node (Atomic.get list_head)
+
+let add_and_check list_head n () =
+  assert(add_node list_head n);
+  assert(check_node list_head n)
+
+let create_test upto =
+  let list_head = Atomic.make { value = 0 ; next = None } in
+  for x = 1 to upto do
+    Atomic.spawn (add_and_check list_head x);
+  done
 
 let () =
-  Atomic.trace do_test
+  create_test 3;
+  Atomic.trace ()
--- src/tracedAtomic.ml
+++ src/tracedAtomic.ml
@@ -4,7 +4,7 @@ open EffectHandlers.Deep
 type 'a t = 'a Atomic.t
 
 type _ eff +=
-    Make : 'a -> 'a Atomic.t eff
+  | Make : 'a -> 'a Atomic.t eff
   | Get : 'a Atomic.t -> 'a eff
   | Set : ('a Atomic.t * 'a) -> unit eff
   | Exchange : ('a Atomic.t * 'a) -> 'a eff
@@ -13,74 +13,152 @@ type _ eff +=
 
 type atomic_op = Make | Get | Set | Exchange | CompareAndSwap | FetchAndAdd
 
-type trace_record = { op: atomic_op; repr: Obj.t; prev: trace_record option }
+type trace_cell = { op : atomic_op; process_id : int; repr : Obj.t }
 
-let trace_head = Atomic.make None
-
-let rec add_trace op repr =
-  let curr_head = Atomic.get trace_head in
-    if not(Atomic.compare_and_set trace_head curr_head (Some { op; repr; prev = curr_head })) then
-      add_trace op repr
-    else
-      ()
-
-let handler = {
-  effc = fun (type a) (e : a eff) ->
-    match e with
-    | Make v -> Some(
-      fun (k : (a, _) continuation) ->
-        let m = Atomic.make v in
-        add_trace Make (Obj.repr m);
-        continue k m
-      )
-    | Get v -> Some(
-        fun (k : (a, _) continuation) ->
-          add_trace Get (Obj.repr v);
-          continue k (Atomic.get v)
-        )
-    | Set (r, v) -> Some(
-        fun (k : (a, _) continuation) ->
-          add_trace Set (Obj.repr r);
-          continue k (Atomic.set r v)
-        )
-    | Exchange (a, b) -> Some(
-        fun (k : (a, _) continuation) ->
-          add_trace Exchange (Obj.repr a);
-          continue k (Atomic.exchange a b)
-        )
-    | CompareAndSwap (x, s, v) -> Some(
-        fun (k : (a, _) continuation) ->
-          add_trace CompareAndSwap (Obj.repr x);
-          continue k (Atomic.compare_and_set x s v)
-        )
-    | FetchAndAdd (v, x) -> Some(
-        fun (k : (a, _) continuation) ->
-          add_trace FetchAndAdd (Obj.repr v);
-          continue k (Atomic.fetch_and_add v x)
-        )
-    | _ -> None
+type process_data = {
+  id : int;
+  mutable event_counter : int;
+  mutable resume_func : unit -> unit;
+  mutable finished : bool;
 }
 
-let trace f =
-  try_with f () handler
+let processes = Hashtbl.create 10
 
-let make v =
-  perform (Make v)
+let current_process = ref 0
 
-let get r =
-  perform (Get r)
+let trace_list = ref []
 
-let set r v =
-  perform (Set (r, v))
+let add_trace op repr =
+  trace_list := { op; process_id = !current_process; repr } :: !trace_list
 
-let[@inline never] exchange r v =
-  perform (Exchange (r, v))
+let update_process_data f =
+  let process_rec = Hashtbl.find processes !current_process in
+  process_rec.resume_func <- f;
+  process_rec.event_counter <- process_rec.event_counter + 1
 
-let[@inline never] compare_and_set r seen v =
-  perform (CompareAndSwap (r, seen, v))
+let finish_process () =
+  let process_rec = Hashtbl.find processes !current_process in
+  process_rec.finished <- true
 
-let[@inline never] fetch_and_add r n =
-  perform (FetchAndAdd (r, n))
+let handler runner =
+  {
+    retc =
+      (fun _ ->
+        (
+          Printf.printf "Finished process %d\n" !current_process;
+        finish_process ();
+        runner ()));
+    exnc = (fun _ -> ());
+    effc =
+      (fun (type a) (e : a eff) ->
+        match e with
+        | Make v ->
+            Some
+              (fun (k : (a, _) continuation) ->
+                Printf.printf "Process %d: Make\n" !current_process;
+                let m = Atomic.make v in
+                add_trace Make (Obj.repr m);
+                update_process_data (fun () -> continue k m);
+                current_process :=
+                  (!current_process + 1) mod Hashtbl.length processes;
+                runner ())
+        | Get v ->
+            Some
+              (fun (k : (a, _) continuation) ->
+                Printf.printf "Process %d: Get\n" !current_process;
+                add_trace Get (Obj.repr v);
+                update_process_data (fun () -> continue k (Atomic.get v));
+                current_process :=
+                  (!current_process + 1) mod Hashtbl.length processes;
+                runner ())
+        | Set (r, v) ->
+            Some
+              (fun (k : (a, _) continuation) ->
+                Printf.printf "Process %d: Set\n" !current_process;
+                add_trace Set (Obj.repr r);
+                update_process_data (fun () -> continue k (Atomic.set r v));
+                current_process :=
+                  (!current_process + 1) mod Hashtbl.length processes;
+                runner ())
+        | Exchange (a, b) ->
+            Some
+              (fun (k : (a, _) continuation) ->
+                Printf.printf "Process %d: Exchange\n" !current_process;
+                add_trace Exchange (Obj.repr a);
+                update_process_data (fun () -> continue k (Atomic.exchange a b));
+                current_process :=
+                  (!current_process + 1) mod Hashtbl.length processes;
+                runner ())
+        | CompareAndSwap (x, s, v) ->
+            Some
+              (fun (k : (a, _) continuation) ->
+                Printf.printf "Process %d: CAS\n" !current_process;
+                add_trace CompareAndSwap (Obj.repr x);
+                update_process_data (fun () ->
+                    continue k (Atomic.compare_and_set x s v));
+                current_process :=
+                  (!current_process + 1) mod Hashtbl.length processes;
+                runner ())
+        | FetchAndAdd (v, x) ->
+            Some
+              (fun (k : (a, _) continuation) ->
+                Printf.printf "Process %d: FetchAndAdd\n" !current_process;
+                add_trace FetchAndAdd (Obj.repr v);
+                update_process_data (fun () ->
+                    continue k (Atomic.fetch_and_add v x));
+                current_process :=
+                  (!current_process + 1) mod Hashtbl.length processes;
+                runner ())
+        | _ ->
+            Printf.printf "Process %d finished\n" !current_process;
+            None);
+  }
+
+let tracing = ref false
+
+let trace () =
+  tracing := true;
+  let num_processes = Hashtbl.length processes in
+  (* do our first trace in a haphazard fashion *)
+  let rec round_robin_run finished_counter () =
+    if finished_counter >= num_processes then (* We are done *)
+      ()
+    else
+      let process_to_run = Hashtbl.find processes !current_process in
+      if process_to_run.finished then (
+        current_process := (!current_process + 1) mod num_processes;
+        round_robin_run (finished_counter + 1) ())
+      else
+        (* process not finished, need to run it*)
+        (Printf.printf "Switching to process %d\n" !current_process;
+        match_with process_to_run.resume_func () (handler (round_robin_run 0)))
+  in
+  round_robin_run 0 ();
+  tracing := false
+
+let make v = if !tracing then perform (Make v) else Atomic.make v
+
+let get r = if !tracing then perform (Get r) else Atomic.get r
+
+let set r v = if !tracing then perform (Set (r, v)) else Atomic.set r v
+
+let exchange r v =
+  if !tracing then perform (Exchange (r, v)) else Atomic.exchange r v
+
+let compare_and_set r seen v =
+  if !tracing then perform (CompareAndSwap (r, seen, v))
+  else Atomic.compare_and_set r seen v
+
+let fetch_and_add r n =
+  if !tracing then perform (FetchAndAdd (r, n)) else Atomic.fetch_and_add r n
 
 let incr r = ignore (fetch_and_add r 1)
+
 let decr r = ignore (fetch_and_add r (-1))
+
+let spawn _f =
+  let new_id = Hashtbl.length processes in
+  Hashtbl.add processes new_id
+    { id = new_id; event_counter = 0; resume_func = _f; finished = false }
+
+let clear () = ()
