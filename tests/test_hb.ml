98b678e87aa687be89c97f3e9b0697a8a26d6fcf
Bartosz Modelski <modelski.bartosz@gmail.com>
2023-04-24 22:54:00 +0100

    tests

---
+++ tests/test_hb.ml
@@ -0,0 +1,11 @@
+module Atomic = Dscheck.TracedAtomic
+
+(* get + get *)
+let test1 () =
+  let a = Atomic.make 0 in
+  Atomic.spawn (fun () -> Atomic.set a 2);
+  Atomic.spawn (fun () -> Atomic.get a |> ignore);
+  Atomic.spawn (fun () -> Atomic.get a |> ignore);
+  ()
+
+let () = Atomic.trace ~interleavings:stdout test1
